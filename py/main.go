1d7432144c102d387b5bd573b3229287fd2fd2a2
Alex Romaniuk <alaster.rom@gmail.com>
2020-05-14 11:59:44 +0300

    add

---
+++ py/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
+
+func test() {
+	fmt.Println("testqweqwe")
+}
+
+func hello(s string) {
+	fmt.Println("hello", s)
+}
+
+func test2() {
+	fmt.Ptintln("test2")
+}
+
+func test3() {
+	fmt.Ptintln("test3")
+}
