cdcba02c6d354f28f983e4859a150b0d83360aec
Alex Romaniuk <alaster.rom@gmail.com>
2020-05-15 13:41:31 +0300

    add 1

---
+++ java/DifNumbers_1.java
@@ -0,0 +1,9 @@
+public class DifNumbers {
+
+   public static void main(String[] args) {
+      int num0 = 3, num2 = 10; 
+	  int sum;
+      sum = num0 + num2;
+      System.out.println("Sum of these numbers: " + sum);
+   }
+}
